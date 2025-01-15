831c607335647f8fcd665a12090a0de2fd3df544
Silvio Balduzzi <silvio.balduzzi@gmail.com>
2025-01-15 18:03:24 +0100

    AULReport 2.3.10 and AULService 3.2.2.1

--- AULMonitor patch v2.1.sql
+++ AULMonitor patch v2.1.sql
@@ -1,31 +1,31 @@
+-- 1. Modifica del tipo di colonna Base64 in dbo.AppIcons
 ALTER TABLE dbo.AppIcons
 ALTER COLUMN Base64 nvarchar(MAX);
 
-UPDATE dbo.AppUserList
-SET Date = CONVERT(varchar(10), CONVERT(datetime, Date, 103), 120)
-WHERE ISDATE(Date) = 1;
-
+-- 2. Aggiunta di una colonna temporanea TempDate per la conversione delle date
 ALTER TABLE dbo.AppUserList
-ALTER COLUMN Date datetime;
+ADD TempDate datetime;
 
+-- 3. Popolamento della colonna temporanea con valori validi convertiti in datetime
+-- Se la conversione fallisce, si può settare NULL o una data predefinita (ad esempio, '1900-01-01')
+UPDATE dbo.AppUserList
+SET TempDate = CASE 
+                   WHEN TRY_CONVERT(datetime, Date, 103) IS NOT NULL THEN TRY_CONVERT(datetime, Date, 103)
+                   ELSE NULL -- Oppure '1900-01-01' per valore predefinito
+               END;
+
+-- 4. Verifica di eventuali valori non convertibili (per eventuale debug)
 SELECT Date
 FROM dbo.AppUserList
 WHERE TRY_CONVERT(datetime, Date, 103) IS NULL;
 
-ALTER TABLE dbo.AppUserList
-ALTER COLUMN Date datetime USING TRY_CONVERT(datetime, Date, 103);
-
-ALTER TABLE dbo.AppUserList
-ADD TempDate datetime;
-
-UPDATE dbo.AppUserList
-SET TempDate = TRY_CONVERT(datetime, Date, 103)
-WHERE TRY_CONVERT(datetime, Date, 103) IS NOT NULL;
-
+-- 5. Rimuove la colonna originale Date dopo la conversione
 ALTER TABLE dbo.AppUserList
 DROP COLUMN Date;
 
+-- 6. Rinomina la colonna temporanea TempDate in Date
 EXEC sp_rename 'dbo.AppUserList.TempDate', 'Date', 'COLUMN';
 
+-- 7. Verifica finale dei dati nella tabella
 SELECT *
 FROM dbo.AppUserList;
